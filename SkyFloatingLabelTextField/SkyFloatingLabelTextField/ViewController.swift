2f0bbabe59c79ba4d82cea107acfa58d3df1222b
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2019-11-21 13:47:38 -0300

    init

---
+++ SkyFloatingLabelTextField/SkyFloatingLabelTextField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyFloatingLabelTextField
+//
+//  Created by Gabriel Sanzone on 21/11/19.
+//  Copyright © 2019 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
